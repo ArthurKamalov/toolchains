82a419ccc4805a162332905a2be54940ba36e7e3
arthur <arthurk@sentinelone.com>
2023-06-02 12:36:23 +0200

    test

---
+++ build/build_toolchain.sh
@@ -0,0 +1,17 @@
+
+SCRIPT_PATH="${0}"
+BUILD_DIR="$(dirname "${SCRIPT_PATH}")"
+SOURCE_ROOT="$(dirname "${BUILD_DIR}")"
+SAMPLE_NAME="${1}"
+
+NAME="toolchain-${SAMPLE_NAME}"
+
+docker buildx build -t "${NAME}" --load --build-arg="SAMPLE_NAME=${SAMPLE_NAME}" "${BUILD_DIR}/docker"
+
+docker rm -f "${NAME}"
+docker create --name "${NAME}" "${NAME}"
+docker cp "${NAME}:/tmp/${SAMPLE_NAME}-native.tar.gz" "${SOURCE_ROOT}"
+docker cp "${NAME}:/tmp/${SAMPLE_NAME}-cross.tar.gz" "${SOURCE_ROOT}"
+
+docker rm "${NAME}"
+
