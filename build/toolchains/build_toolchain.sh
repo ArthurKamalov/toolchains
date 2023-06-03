30d72610455c9ed59ab9fbc55b8f29c8348b3aeb
arthur <arthurk@sentinelone.com>
2023-06-03 15:15:41 +0200

    test11

--- build/toolchains/build_toolchain.sh
+++ build/toolchains/build_toolchain.sh
@@ -1,37 +1,48 @@
+set -e
 
 SCRIPT_PATH="$(realpath "${0}")"
 PARENT_DIR="$(dirname "${SCRIPT_PATH}")"
-BUILD_DIR="$(dirname "${PARENT_DIR}")"
 SAMPLE_NAME="${1}"
-DISTRO="${2}"
 
 NAME="toolchain-${SAMPLE_NAME}"
 
-GHA_SCOPE="toolchain-7"
+case "$SAMPLE_NAME" in
+  *x86_64*)
+    TOOLCHAIN_PLATFORM="linux/arm64"
+    ;;
+  *aarch64*)
+    TOOLCHAIN_PLATFORM="linux/arm64/v8"
+  ;;
+  *armv7*)
+    TOOLCHAIN_PLATFORM="linux/arm/v7"
+  ;;
+  *)
+    echo -e "Can not determine CPU architecture by SAMPLE_NAME: '${SAMPLE_NAME}'"
+    exit 1
+  ;;
+esac
 
-CROSSTOOLS_NG_IMAGE_NAME="crosstools_ng"
+case "$SAMPLE_NAME" in
+  *-gnu*)
+    DISTRO="ubuntu"
+    ;;
+  *-musl*)
+    DISTRO="alpine"
+  ;;
+  *)
+    echo -e "Can not determine distro by SAMPLE_NAME: '${SAMPLE_NAME}'"
+    exit 1
+  ;;
+esac
 
-OCI_IMAGES_PATH="${PARENT_DIR}/~build"
-rm -rf "${OCI_IMAGES_PATH}"
-mkdir -p "${OCI_IMAGES_PATH}"
-CROSSTOOLS_NG_OCI_IMAGE_PATH="${OCI_IMAGES_PATH}/crosstools_ng"
-CROSSTOOLS_NG_OCI_IMAGE_TARBALL_PATH="${CROSSTOOLS_NG_OCI_IMAGE_PATH}.tar"
+GHA_SCOPE="toolchain-8"
 
-bash "${BUILD_DIR}/crosstools-ng/build_crosstools_ng.sh" \
-  -t "${CROSSTOOLS_NG_IMAGE_NAME}" \
-  --output "type=oci,dest=${CROSSTOOLS_NG_OCI_IMAGE_TARBALL_PATH}"
-
-mkdir -p "${CROSSTOOLS_NG_OCI_IMAGE_PATH}"
-tar -xf "${CROSSTOOLS_NG_OCI_IMAGE_TARBALL_PATH}" -C "${CROSSTOOLS_NG_OCI_IMAGE_PATH}"
-
-docker buildx build -t "${NAME}" --load \
+docker buildx build -t "${NAME}" \
   --build-arg="SAMPLE_NAME=${SAMPLE_NAME}" \
   --build-arg="DISTRO=${DISTRO}" \
+  --build-arg="TOOLCHAIN_TYPE=cross" \
+  --build-arg="TOOLCHAIN_PLATFORM=${TOOLCHAIN_PLATFORM}" \
   --cache-from="type=gha,scope=${GHA_SCOPE}-${SAMPLE_NAME}" \
   --cache-to=type="gha,mode=min,scope=${GHA_SCOPE}-${SAMPLE_NAME}" \
-  --build-context "crosstools_ng=oci-layout://${CROSSTOOLS_NG_OCI_IMAGE_PATH}" \
-  "${PARENT_DIR}" --progress=plain
-
-
-rm -rf "${OCI_IMAGES_PATH}"
+  "${PARENT_DIR}" --progress=plain --load
 
