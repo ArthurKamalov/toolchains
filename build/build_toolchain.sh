b350b5234843b6656c5dbb9185f7838ac26954a5
arthur <arthurk@sentinelone.com>
2023-06-03 11:12:37 +0200

    test11

--- build/build_toolchain.sh
+++ build/build_toolchain.sh
@@ -6,7 +6,7 @@ SAMPLE_NAME="${1}"
 
 NAME="toolchain-${SAMPLE_NAME}"
 
-CROSSTOOLS_SCOPE="crosstools-ng-3"
+CROSSTOOLS_SCOPE="crosstools-ng-4"
 
 docker buildx build -t "crossstools-ng" --load \
   --cache-from="type=gha,scope=$CROSSTOOLS_SCOPE" \
