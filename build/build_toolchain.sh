76907f72fdae9be7c77a9e628ac57ea417752336
arthur <arthurk@sentinelone.com>
2023-06-03 10:39:33 +0200

    test11

--- build/build_toolchain.sh
+++ build/build_toolchain.sh
@@ -6,9 +6,11 @@ SAMPLE_NAME="${1}"
 
 NAME="toolchain-${SAMPLE_NAME}"
 
+CROSSTOOLS_SCOPE="crosstools-ng-1"
+
 docker buildx build -t "crossstools-ng" --load \
-  --cache-from="type=gha,scope=crosstools-ng" \
-  --cache-to=type="gha,mode=min,scope=crosstools-ng" \
+  --cache-from="type=gha,scope=$CROSSTOOLS_SCOPE" \
+  --cache-to=type="gha,mode=min,scope=$CROSSTOOLS_SCOPE" \
   "${BUILD_DIR}/docker/crosstools-ng"
 
 
