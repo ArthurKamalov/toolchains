46c844b5d58bcb82ffea1fe1850599c3acec01f0
arthur <arthurk@sentinelone.com>
2023-06-03 10:22:25 +0200

    test11

--- build/build_toolchain.sh
+++ build/build_toolchain.sh
@@ -6,15 +6,21 @@ SAMPLE_NAME="${1}"
 
 NAME="toolchain-${SAMPLE_NAME}"
 
-docker buildx build -t "${NAME}" --load --build-arg="SAMPLE_NAME=${SAMPLE_NAME}" \
-  --cache-from="type=gha,scope=$GITHUB_REF_NAME-${SAMPLE_NAME}-1" \
-  --cache-to=type="gha,mode=min,scope=$GITHUB_REF_NAME-${SAMPLE_NAME}-1" \
-  "${BUILD_DIR}/docker"
+docker buildx build -t "crossstools-ng" --load \
+  --cache-from="type=gha,scope=crosstools-ng" \
+  --cache-to=type="gha,mode=min,scope=crosstools-ng" \
+  "${BUILD_DIR}/docker/crosstools-ng"
 
-docker rm -f "${NAME}"
-docker create --name "${NAME}" "${NAME}"
-docker cp "${NAME}:/tmp/${SAMPLE_NAME}-native.tar.gz" "${SOURCE_ROOT}"
-docker cp "${NAME}:/tmp/${SAMPLE_NAME}-cross.tar.gz" "${SOURCE_ROOT}"
 
-docker rm "${NAME}"
+#docker buildx build -t "${NAME}" --load --build-arg="SAMPLE_NAME=${SAMPLE_NAME}" \
+#  --cache-from="type=gha,scope=$GITHUB_REF_NAME-${SAMPLE_NAME}-1" \
+#  --cache-to=type="gha,mode=min,scope=$GITHUB_REF_NAME-${SAMPLE_NAME}-1" \
+#  "${BUILD_DIR}/docker"
+
+#docker rm -f "${NAME}"
+#docker create --name "${NAME}" "${NAME}"
+#docker cp "${NAME}:/tmp/${SAMPLE_NAME}-native.tar.gz" "${SOURCE_ROOT}"
+#docker cp "${NAME}:/tmp/${SAMPLE_NAME}-cross.tar.gz" "${SOURCE_ROOT}"
+#
+#docker rm "${NAME}"
 
